f701aaa9e139008a23c2e8de40d2ebb3f9213725
ChaiOne <chaione@HUSSAINs-MacBook-Pro.local>
2022-08-03 21:10:07 -0500

    Added makefiles to make life easier

---
+++ src/services/alerts/alertsService.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/hnabbasi/gowxapi/models"
+)
+
+const (
+	baseURL     = "https://api.weather.gov"
+	stateAlerts = baseURL + "/alerts/active/area"
+)
+
+func GetAlerts(state string) (models.AlertResponse, error) {
+	url := fmt.Sprintf("%v/%v", stateAlerts, strings.ToUpper(state))
+	response, err := getHttpResponse(url)
+
+	var alertResponse struct {
+		Updated time.Time      `json:"updated"`
+		Alerts  []models.Alert `json:"features"`
+	}
+
+	if jsonErr := json.Unmarshal(response, &alertResponse); jsonErr != nil {
+		log.Fatal(jsonErr)
+		return models.AlertResponse{}, errors.New(jsonErr.Error())
+	}
+	return models.AlertResponse{Updated: alertResponse.Updated, Alerts: alertResponse.Alerts}, err
+}
+
+func getHttpResponse(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(string(body))
+	}
+
+	return body, nil
+}
